0fd027a9ec652f7ef7de9dc50fc01d6ccc4b7d12
Paul <paul@dotvector.com>
2023-01-20 09:03:10 -0600

    laravel sanctum work initial commit

---
+++ laravel_sanctum/app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    use HttpResponses;
+
+    public function login()
+    {
+        return 'This is my login method.';
+    }
+
+    public function register()
+    {
+        return response()->json('This is my register method.');
+    }
+
+    public function logout()
+    {
+        return response()->json('This is my logout method.');
+    }
+}
